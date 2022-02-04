7fb2ec0623e53717c404c77b70b3ca4cf92ee217
mrmomenticus <vova.bod53@gmail.com>
2022-02-04 20:05:27 +0300

    #1 Создание потока 1 и общего буффера.

--- OneStream.cpp
+++ OneStream.cpp
@@ -7,12 +7,23 @@
 //-all_of проверяет заданное свойство для каждого элемента и возвращает true,
 //когда каждый элемент в диапазоне удовлетворяет указанному свойству
 //-isspace проверяет если пользователь ввел только пробел
-//isdigit проверяет то что символ число
+//isdigit проверяет то что символ цифра
+
 bool OneStream::inputValidation(std::string inputString) { 
 	return std::all_of(m_inputString.begin(), m_inputString.end(), [](int ch) {
-		return (std::isdigit(ch) || std::isspace(ch));
+		return (std::isdigit(ch));
 		});
 };
+bool comp(int a, int b) { return (a > b); }
+void OneStream::sortAndReplacement() {
+	std::sort(m_inputString.begin(), m_inputString.end(), comp);
+	/*std::replace_if(m_inputString.begin(), m_inputString.end(), [](const char* c) {return *c == '31'; }, "KB");*/ //попроовать потом реализовать через это
+	for (char& c : m_inputString) {
+		if (c == 50 || c == 52 || c == 54 || c == 56 || c == 58) {
+			m_inputString.replace(m_inputString.find(c), 1, "KB");
+		}
+	};
+}
 void OneStream::quantityCheck() {//проверка на длинну строки, если слишком длинная , то строка очищается и пользователь вводит еще раз
 	if (m_inputString.size() > 64) {
 		std::cout << "Строка слишком длинная! ";
@@ -26,4 +37,9 @@ void OneStream::inputValue() { //отвечает за ввод значений
 		std::getline(std::cin, m_inputString);
 	} while (!inputValidation(m_inputString));
 	quantityCheck();
+	sortAndReplacement();
+	std::cout << m_inputString;
 }
+std::vector <char> OneStream::bufferFilling(std::vector<char> buffer) {
+	
+};
--- OneStream.h
+++ OneStream.h
@@ -1,14 +1,16 @@
 #pragma once
 #include <string>
+#include <vector>
 class OneStream
 {
 	std::string m_inputString;
 	bool inputValidation(std::string inputString);
 	void quantityCheck();
+	void sortAndReplacement();
 public:
 	OneStream(std::string inputString) : m_inputString(inputString) {
 	}
 	void inputValue();
-	
+	std::vector<char> bufferFilling(std::vector<char> buffer) {};
 };
 
