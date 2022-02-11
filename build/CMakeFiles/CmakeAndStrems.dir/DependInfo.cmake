1b958232fa1c39c88ecdaafcb49df07348698996
1moment <vova.bod53@gmail.com>
2022-02-11 15:38:09 +0300

    #5 Собран билд

---
+++ build/CMakeFiles/CmakeAndStrems.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/moment/CLionProjects/CmakeAndStrems/OneStream.cpp" "/home/moment/CLionProjects/CmakeAndStrems/build/CMakeFiles/CmakeAndStrems.dir/OneStream.cpp.o"
+  "/home/moment/CLionProjects/CmakeAndStrems/TestProg1.cpp" "/home/moment/CLionProjects/CmakeAndStrems/build/CMakeFiles/CmakeAndStrems.dir/TestProg1.cpp.o"
+  "/home/moment/CLionProjects/CmakeAndStrems/TwoStream.cpp" "/home/moment/CLionProjects/CmakeAndStrems/build/CMakeFiles/CmakeAndStrems.dir/TwoStream.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/CmakeAndStrems.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CmakeAndStrems.dir/OneStream.cpp.o"
+  "CMakeFiles/CmakeAndStrems.dir/TestProg1.cpp.o"
+  "CMakeFiles/CmakeAndStrems.dir/TwoStream.cpp.o"
+  "CmakeAndStrems"
+  "CmakeAndStrems.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CmakeAndStrems.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
